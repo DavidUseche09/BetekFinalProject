991574df3525259c7c5c5decd3e488e5e77a1f3d
DavidUseche09 <daviduseche09@gmail.com>
2023-12-18 16:32:34 -0500

    First steps

---
+++ microservicioRegistro/src/main/java/com/makaia/modRegistro/microservicioRegistro/MicroservicioRegistroApplication.java
@@ -0,0 +1,13 @@
+package com.makaia.modRegistro.microservicioRegistro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioRegistroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioRegistroApplication.class, args);
+	}
+
+}
---
+++ microservicioRegistro/src/test/java/com/makaia/modRegistro/microservicioRegistro/MicroservicioRegistroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makaia.modRegistro.microservicioRegistro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioRegistroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
