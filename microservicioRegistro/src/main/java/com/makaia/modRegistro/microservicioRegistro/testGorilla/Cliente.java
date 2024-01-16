dc32bd156795482b6b6c297b24db6b725f149c95
DavidUseche09 <daviduseche09@gmail.com>
2024-01-16 15:09:47 -0500

    Agregando dependencias swagger

---
+++ microservicioRegistro/src/main/java/com/makaia/modRegistro/microservicioRegistro/testGorilla/Cliente.java
@@ -0,0 +1,12 @@
+package com.makaia.modRegistro.microservicioRegistro.testGorilla;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.client.RestTemplate;
+
+public class Cliente {
+    public String enviarAssesment(){
+        RestTemplate lugel = new RestTemplate();
+        lugel.put("https://app.testgorilla.com/api/assessments/<ASSESSMENT_ID>/", @RequestBody hola);
+        reutrn
+    }
+}
