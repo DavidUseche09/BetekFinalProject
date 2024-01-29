6a0ca97d9a202c3c4f7bc5d622d76313fed5ebea
flsierra <fabiansierra09@hotmail.com>
2024-01-29 16:19:11 -0500

    jwt is added and the candidate and results table is fixed

--- microservicioRegistro/src/main/java/com/makaia/modRegistro/microservicioRegistro/Entities/Aspirante.java
+++ microservicioRegistro/src/main/java/com/makaia/modRegistro/microservicioRegistro/Entities/Aspirante.java
@@ -73,7 +73,7 @@ public class Aspirante {
     Entrenamiento entrenamiento;
     @ManyToOne (optional = false)
     Estrato estrato;
-    @ManyToOne (optional = false)
+    @ManyToOne ()
     ResultadosTestGorilla resultadosTestGorilla;
 
 
