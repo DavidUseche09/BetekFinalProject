9f8cc54d2f5400290bc0170904de6b47083e135e
DavidUseche09 <daviduseche09@gmail.com>
2024-01-30 17:23:06 -0500

    Swagger test done

--- microservicioRegistro/src/main/java/com/makaia/modRegistro/microservicioRegistro/Swagger/SwaggerConfiguration.java
+++ microservicioRegistro/src/main/java/com/makaia/modRegistro/microservicioRegistro/Swagger/SwaggerConfiguration.java
@@ -1,20 +1,28 @@
 package com.makaia.modRegistro.microservicioRegistro.Swagger;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@SecurityScheme(
+        // JWT configuration
+        type = SecuritySchemeType.HTTP,
+        name = "BearerAuth",
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 @OpenAPIDefinition(
         info = @Info(
                 title = "Microservicio Registro API"
                 ),
         servers = {
                 @Server(url = "htpp://prueba/", description = "Servidor de produccion"),
-                @Server(url = "htpp://localhost:8081/", description = "Servidor local")
+                @Server(url = "htpp://localhost:8081/api/v1/", description = "Servidor local")
                 }
 )
-
 public class SwaggerConfiguration {
 }
--- microservicioRegistro/src/main/java/com/makaia/modRegistro/microservicioRegistro/testGorilla/TestGorillaService.java
+++ microservicioRegistro/src/main/java/com/makaia/modRegistro/microservicioRegistro/testGorilla/TestGorillaService.java
@@ -14,7 +14,7 @@ import java.util.Collections;
 import java.util.List;
 
 @Service
-public class TestGorillaService {
+public class TestGorillaService  {
 
     private final RestTemplate restTemplate;
 
