331e68be69abb8dee9f4d5403a87765e59fce20b
DavidUseche09 <daviduseche09@gmail.com>
2024-01-29 20:32:30 -0500

    Agregando variables de entorno

--- microservicioRegistro/src/main/java/com/makaia/modRegistro/microservicioRegistro/testGorilla/TestGorillaService.java
+++ microservicioRegistro/src/main/java/com/makaia/modRegistro/microservicioRegistro/testGorilla/TestGorillaService.java
@@ -23,24 +23,16 @@ public class TestGorillaService {
         this.restTemplate = restTemplate;
     }
 
-    // End-point para invitar a un aspirante por correo
-    @Value("${https://app.testgorilla.com/api}")
-    private String emailInvitationUrl;
-
-    // End-point para listar candidatos invitados
-    @Value("${https://app.testgorilla.com/api}")
-    private String listCandidatesUrl;
-
-    // End-point para recibir resultados de los aspirantes
-    @Value("${https://app.testgorilla.com/api}")
-    private String testResultUrl;
+    // End-point de testGorilla
+    @Value("${testGorillaUrl}")
+    private String testGorillaUrl;
 
     // End-point del token imitador
     @Value("-H Authorization: Token YOUR_FAKE_TOKEN")
     private String testGorillaApiToken;
 
     public InvitationResponse inviteCandidate(String assessmentId, InvitationRequest invitationRequest) {
-        String url = emailInvitationUrl + "/assessments/" + assessmentId + "/invite_candidate/";
+        String url = testGorillaUrl + "/assessments/" + assessmentId + "/invite_candidate/";
         HttpHeaders headers = new HttpHeaders();
         headers.set("Content-Type", "application/json");
         headers.set("Authorization", "Token " + testGorillaApiToken);
@@ -57,7 +49,7 @@ public class TestGorillaService {
     }
 
     public List<TestGorillaCandidateDTO> getInvitedCandidates(String assessmentId) {
-        String url = listCandidatesUrl + "/assessments/candidature/?assessment=" + assessmentId;
+        String url = testGorillaUrl + "/assessments/candidature/?assessment=" + assessmentId;
 
         HttpHeaders headers = new HttpHeaders();
         headers.set("Authorization", "Token " + testGorillaApiToken);
@@ -75,7 +67,7 @@ public class TestGorillaService {
     }
 
     public List<TestGorillaResultDTO> getAssessmentResults(String assessmentId, String testTakerId) {
-        String url = testResultUrl + "/assessments/results/?candidature__assessment=" + assessmentId
+        String url = testGorillaUrl + "/assessments/results/?candidature__assessment=" + assessmentId
                 + "&candidature__test_taker=" + testTakerId;
 
         HttpHeaders headers = new HttpHeaders();
