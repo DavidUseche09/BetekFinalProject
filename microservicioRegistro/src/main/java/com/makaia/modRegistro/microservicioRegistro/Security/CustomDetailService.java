4dafb04e258152b59155efad343806dc5fb3187b
DavidUseche09 <daviduseche09@gmail.com>
2024-02-05 17:19:48 -0500

    saving changes

--- microservicioRegistro/src/main/java/com/makaia/modRegistro/microservicioRegistro/Security/CustomDetailService.java
+++ microservicioRegistro/src/main/java/com/makaia/modRegistro/microservicioRegistro/Security/CustomDetailService.java
@@ -1,46 +1,46 @@
-package com.makaia.modRegistro.microservicioRegistro.Security;
-
-import com.makaia.modRegistro.microservicioRegistro.Entities.Roles;
-import com.makaia.modRegistro.microservicioRegistro.Entities.Usuario;
-import com.makaia.modRegistro.microservicioRegistro.Repositories.RolesRepository;
-import com.makaia.modRegistro.microservicioRegistro.Repositories.UsuarioRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-@Service
-public class CustomDetailService implements UserDetailsService {
-@Autowired
-    UsuarioRepository usuarioRepository;
-
-@Override
-public UserDetails loadUserByUsername (String email) throws UsernameNotFoundException{
-    Optional<Usuario> user = this.usuarioRepository.findByEmail(email);
-    if(user== null){
-        throw new UsernameNotFoundException(email);
-    }
-    List<Roles> rol = new ArrayList<>();
-     rol.add(new Roles(user.getRol().getDescripcion()));
-    UserDetails userCreate = User.withUsername(user.getEmail())
-            .password(user.getPassword())
-            .roles("ROLE_"+user.getRol().getDescripcion())
-            .build();
-    System.out.println(userCreate);
-    return userCreate;
-
-
-}
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder(8);
-    }
-}
+//package com.makaia.modRegistro.microservicioRegistro.Security;
+//
+//import com.makaia.modRegistro.microservicioRegistro.Entities.Roles;
+//import com.makaia.modRegistro.microservicioRegistro.Entities.Usuario;
+//import com.makaia.modRegistro.microservicioRegistro.Repositories.RolesRepository;
+//import com.makaia.modRegistro.microservicioRegistro.Repositories.UsuarioRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Service
+//public class CustomDetailService implements UserDetailsService {
+//@Autowired
+//    UsuarioRepository usuarioRepository;
+//
+//@Override
+//public UserDetails loadUserByUsername (String email) throws UsernameNotFoundException{
+//    Optional<Usuario> user = this.usuarioRepository.findByEmail(email);
+//    if(user== null){
+//        throw new UsernameNotFoundException(email);
+//    }
+//    List<Roles> rol = new ArrayList<>();
+//     rol.add(new Roles(user.getRol().getDescripcion()));
+//    UserDetails userCreate = User.withUsername(user.getEmail())
+//            .password(user.getPassword())
+//            .roles("ROLE_"+user.getRol().getDescripcion())
+//            .build();
+//    System.out.println(userCreate);
+//    return userCreate;
+//
+//
+//}
+//    public PasswordEncoder passwordEncoder(){
+//        return new BCryptPasswordEncoder(8);
+//    }
+//}
